24f53937b9606f4c14c61edb49a8ea2c4c8d6225
MixxaVega <wendyv.vega@gmail.com>
2018-11-06 13:40:19 -0700

    creacion de la vista principal

--- multisensoryStake/app/src/main/java/com/example/supdude/multisensorystake/MainActivity.java
+++ multisensoryStake/app/src/main/java/com/example/supdude/multisensorystake/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.supdude.multisensorystake;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
+
+    Button btnInfo;
+    Button btnDatos;
+    Button btnHistorial;
+    ImageView btnBlue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnInfo = findViewById(R.id.btnInformacion);
+        btnDatos = findViewById(R.id.btnDatos);
+        btnHistorial = findViewById(R.id.btnHistorial);
+        btnBlue = findViewById(R.id.btnBlue);
+
+        btnInfo.setOnClickListener(this);
+        btnDatos.setOnClickListener(this);
+        btnHistorial.setOnClickListener(this);
+        btnBlue.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnInformacion:
+               // Intent i = new Intent(this, informacion.class);
+               // startActivity(i);
+                break;
+            case R.id.btnDatos:
+                 Intent i = new Intent(this, datos.class);
+                 startActivity(i);
+                break;
+            case R.id.btnHistorial:
+                // Intent i = new Intent(this, informacion.class);
+                // startActivity(i);
+                break;
+            case R.id.btnBlue:
+                break;
+        }
+
     }
 }
